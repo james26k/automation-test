066f66ee1f95346f4a9a73782e5fa0444c0e4377
james26k <cb26.hayashi@gmail.com>
2020-12-23 18:55:28 +0900

    add: automation test project

---
+++ automation-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  automation-test
+//
+//  Created by Kohei Hayashi on 2020/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

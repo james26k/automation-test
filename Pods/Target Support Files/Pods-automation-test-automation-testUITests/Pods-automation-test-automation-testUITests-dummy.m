3bb31c8ac93d034059a810a3ad9139ec276ee81e
james26k <cb26.hayashi@gmail.com>
2020-12-23 19:37:00 +0900

    remove: Podsディレクトリをgitの管理対象外とする

--- Pods/Target Support Files/Pods-automation-test-automation-testUITests/Pods-automation-test-automation-testUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_automation_test_automation_testUITests : NSObject
-@end
-@implementation PodsDummy_Pods_automation_test_automation_testUITests
-@end

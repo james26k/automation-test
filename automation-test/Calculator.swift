b8827c216bf631cd0b9304c78650277322c226c8
james26k <cb26.hayashi@gmail.com>
2020-12-23 21:54:55 +0900

    add: 四則演算のメソッドテストを追加

--- automation-test/Calculator.swift
+++ automation-test/Calculator.swift
@@ -8,7 +8,20 @@
 import Foundation
 
 final class Calculator {
-    func sum(_ arg1: Int, _ arg2: Int) -> Int {
+    func add(_ arg1: Int, _ arg2: Int) -> Int {
         arg1 + arg2
+    }
+
+    func subtract(_ arg1: Int, _ arg2: Int) -> Int {
+        arg1 - arg2
+    }
+
+    func multiple(_ arg1: Int, _ arg2: Int) -> Int {
+        arg1 * arg2
+    }
+
+    func division(_ arg1: Int, _ arg2: Int) -> Int? {
+        let divisionInt = arg1 / arg2
+        return divisionInt != 0 ? divisionInt : nil
     }
 }
--- automation-testTests/QuickCalculatorTests.swift
+++ automation-testTests/QuickCalculatorTests.swift
@@ -13,14 +13,40 @@ import Nimble
 final class QuickCalculatorTests: QuickSpec {
     override func spec() {
         var calculator: Calculator!
-
+        // NOTE: call next closure whenever `it` will be called.
         beforeEach {
             calculator = Calculator()
         }
 
-        describe("sum(_:_:)") {
+        describe("add(_:_:)") {
             it("1 + 2 = 3") {
-                expect(calculator.sum(1, 2)).to(equal(3))
+                expect(calculator.add(1, 2)).to(equal(3))
+            }
+        }
+
+        describe("subtract(_:_:)") {
+            it("3 - 2 = 1") {
+                expect(calculator.subtract(3, 2)).to(equal(1))
+            }
+        }
+
+        describe("multiple(_:_:)") {
+            it("2 * 3 = 6") {
+                expect(calculator.multiple(2, 3)).to(equal(6))
+            }
+        }
+
+        describe("division(_:_:)") {
+            context("8 / 2") {
+                it("return 4") {
+                    expect(calculator.division(8, 2)).to(equal(4))
+                }
+            }
+
+            context("8 / 0") {
+                it("return nil") {
+                    expect(calculator.division(8, 0)).to(beNil())
+                }
             }
         }
     }
